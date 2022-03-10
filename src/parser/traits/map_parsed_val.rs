a74134316320b9e8288721a9aefab45725c82bf2
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-03-10 00:15:59 -0700

    Added MapParsedValInResult

--- src/parser/traits/map_parsed_val.rs
+++ src/parser/traits/map_parsed_val.rs
@@ -1,4 +1,8 @@
-pub trait MapParsedVal<'a,T>
+// Proper declaration and implementation requires GATs
+
+use nom::IResult;
+
+pub trait MapParsedValInTuple<'a,T>
     where
         T: super::Parse<'a>
 {
@@ -7,7 +11,7 @@ pub trait MapParsedVal<'a,T>
         'a: 'b;
 }
 
-impl<'a,T> MapParsedVal<'a,T> for (&'a str,T)
+impl<'a,T> MapParsedValInTuple<'a,T> for (&'a str,T)
     where
         T: super::Parse<'a>
 {
@@ -16,5 +20,26 @@ impl<'a,T> MapParsedVal<'a,T> for (&'a str,T)
         'a: 'b
     {
         (self.0,f(self.1))
+    }
+}
+
+pub trait MapParsedValInResult<'a,T>
+    where
+        T: super::Parse<'a>
+{
+    fn map_parsed_val<'b,U,F: FnOnce(T) -> U>(self,f: F) -> IResult<&'b str, U>
+    where
+        'a: 'b;
+}
+
+impl<'a,T> MapParsedValInResult<'a,T> for IResult<&'a str, T>
+    where
+        T: super::Parse<'a>
+{
+    fn map_parsed_val<'b,U,F: FnOnce(T) -> U>(self,f: F) -> IResult<&'b str, U>
+    where
+        'a: 'b
+    {
+        self.map(|parsed_pair| parsed_pair.map_parsed_val(f))
     }
 }
