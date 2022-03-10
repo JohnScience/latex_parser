f283fec2b7be82291ebdf416c9a516b21c27be7c
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-03-09 23:54:31 -0700

    Documented crate::parser::traits::Parse::parse

--- src/parser/traits/parse.rs
+++ src/parser/traits/parse.rs
@@ -1,6 +1,9 @@
 use nom::{combinator::opt, IResult};
 
 pub trait Parse<'a>: Sized {
+    /// Returns [`Result<P,E>`] where any [`Ok(p)`] is a pair `(i,val)`, s.t.
+    /// * `i` is the remaining input after parsing
+    /// * `val` is the parsed value
     fn parse<'b, 'c>(i: &'b str) -> IResult<&'c str, Self>
     where
         'b: 'c,
